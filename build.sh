4f154ddfb3921f0da30214cff6ed1cb69da9908e
Evan Zacks <evan.zacks@stitchfix.com>
2025-01-24 09:01:03 -0800

    add buildspec and script to publish to s3

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+#DRY_RUN="--dryrun"
+
+SUBDIR=""
+
+if [ "$CODEBUILD_WEBHOOK_TRIGGER" != "branch/main" ]; then
+  SUBDIR="__$CODEBUILD_WEBHOOK_TRIGGER"
+  TARGET="s3://fo-stitchfix-algorithms-tour-production/$SUBDIR"
+else
+  TARGET="s3://fo-stitchfix-algorithms-tour-production"
+fi
+
+for DIR in css img js svg; do
+  aws s3 cp --recursive \
+    $DRY_RUN \
+    --cache-control max-age=31104000 \
+    $DIR \
+    $TARGET/$DIR
+done
+
+# lower cache time for index.html
+aws s3 cp \
+  $DRY_RUN \
+  --cache-control max-age=300 \
+  index.html \
+  $TARGET/index.html
